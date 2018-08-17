f2873a0988e5f21a5d35fc4c0336fda63c5381ed
ricmoreira <ricardo.moreira@jumia.com>
2018-08-17 11:47:37 +0100

    no transformation of backend responses

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"time"
 
@@ -29,6 +31,28 @@ func main() {
 	if err != nil {
 		log.Fatal("ERROR:", err.Error())
 	}
+
+	// render that does not change response
+	noTransformRender := func(c *gin.Context, response *proxy.Response) {
+		if response == nil {
+			c.Status(http.StatusInternalServerError)
+			return
+		}
+		c.Status(response.Metadata.StatusCode)
+		for k, v := range response.Metadata.Headers {
+			c.Header(k, v[0])
+		}
+		io.Copy(c.Writer, response.Io)
+	}
+
+	// register the render at the router level
+	krakendgin.RegisterRender("NoTransformRender", noTransformRender)
+
+	// assign NoTransformRender to all endpoints loaded from config file
+	for _, v := range serviceConfig.Endpoints {
+		v.OutputEncoding = "NoTransformRender"
+	}
+
 	serviceConfig.Debug = serviceConfig.Debug || *debug
 	if *port != 0 {
 		serviceConfig.Port = *port
@@ -40,14 +64,34 @@ func main() {
 		return
 	}
 
-	routerFactory := krakendgin.NewFactory(krakendgin.Config{
-		Engine:         gin.Default(),
-		ProxyFactory:   customProxyFactory{logger, proxy.DefaultFactory(logger)},
+	backendFactory := func(backendCfg *config.Backend) proxy.Proxy {
+
+		// status handler that does change status
+		ns := proxy.NoOpHTTPStatusHandler
+
+		// the default request executor
+		re := proxy.DefaultHTTPRequestExecutor(proxy.NewHTTPClient)
+
+		// response parser that copies Backend response body to proxy Response IO reader
+		rp := proxy.NoOpHTTPResponseParser
+
+		// build and return the new backend proxy
+		return proxy.NewHTTPProxyDetailed(backendCfg, re, ns, rp)
+	}
+
+	// build the pipes on top of the custom backend factory
+	proxyFactory := proxy.NewDefaultFactory(backendFactory, logger)
+
+	engine := gin.Default()
+
+	routerConfig := krakendgin.Config{
+		Engine:         engine,
+		ProxyFactory:   proxyFactory,
 		Logger:         logger,
 		HandlerFactory: krakendgin.EndpointHandler,
 		Middlewares: []gin.HandlerFunc{
 			cors.New(cors.Config{
-				AllowOrigins: []string{"http://localhost:4200", "http://127.0.0.1:4200", "http://localhost:8089", "http://localhost:8069", "http://localhost:8080"},
+				AllowOrigins: []string{"http://localhost:4200", "http://127.0.0.1:4200", "http://localhost:8089", "http://localhost:8069", "http://localhost:8080", "http://localhost:8099"},
 				AllowMethods: []string{"PUT", "PATCH", "POST", "GET", "DELETE", "OPTIONS"},
 				AllowHeaders: []string{"Accept",
 					"Accept-Encoding",
@@ -73,22 +117,9 @@ func main() {
 			}),
 			middleware.JwtCheck(),
 		},
-	})
+	}
+
+	routerFactory := krakendgin.NewFactory(routerConfig)
 
 	routerFactory.New().Run(serviceConfig)
-}
-
-// customProxyFactory adds a logging middleware wrapping the internal factory
-type customProxyFactory struct {
-	logger  logging.Logger
-	factory proxy.Factory
-}
-
-// New implements the Factory interface
-func (cf customProxyFactory) New(cfg *config.EndpointConfig) (p proxy.Proxy, err error) {
-	p, err = cf.factory.New(cfg)
-	if err == nil {
-		p = proxy.NewLoggingMiddleware(cf.logger, cfg.Endpoint)(p)
-	}
-	return
 }
